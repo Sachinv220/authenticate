117845d4abffcfd79368c74d0881d33e8f83591a
Sachin <vsachin220@gmail.com>
2022-09-22 13:20:22 +0530

    small chenges

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 
+app_name = "authenticate"
+
 urlpatterns = [
-    path("login/", views.login_user), 
-    path("signup/", views.signup_user), 
+    path("", views.empty, name="base"), 
+    path("login/", views.login_user, name="login"), 
+    path("signup/", views.signup_user, name="signup"), 
 ]
--- authenticate/views.py
+++ authenticate/views.py
@@ -1,29 +1,48 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
+from django.contrib import messages
 
-redirect_url = "authenticate:root"
+redirect_url = "view-name"
+link = "{% url 'authenticate:login '%}"
+message = " have an account"
 
-@login_required
+
+
 def empty(request):
-    pass
+    if request.user.is_authenticated:
+        return redirect(reverse(redirect_url))
+    return redirect(reverse("authenticate:login"))
+
+def signup_user(request):
+    if request.user.is_authenticated:
+        return redirect(reverse(redirect_url))
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.create_user(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect(reverse(redirect_url))
+        except Exception:
+            messages.add_message(request, messages.ERROR, "your username is already taken")
+    return render(request, "auth.html", context={"title":"Signup", "message": message, "link":"{% url'authenticate:login' %}" ,"final":"login", "login":False})
 
 def login_user(request):
+    if request.user.is_authenticated:
+        return redirect(reverse(redirect_url))
     if request.POST:
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
         if user:
             login(request, user)
-    return render(request, "auth.html", context={"title":"Login", "message":"login"})
+            return redirect(reverse(redirect_url))
+        else:
+            messages.add_message(request, messages.ERROR, "your username or password is incorrect")
+
+    return render(request, "auth.html", context={"title":"Login", "message": "don't " + message, "final": "signup", "login": True})
 
 
-def signup_user(request):
-    if request.POST:
-        username = request.POST['username']
-        password = request.POST['password']
-        user = User.objects.create_user(username=username, password=password)
-        if user:
-            login(request, user)
-    return render(request, "auth.html", context={"title":"Signup", "message":"Signup"})
